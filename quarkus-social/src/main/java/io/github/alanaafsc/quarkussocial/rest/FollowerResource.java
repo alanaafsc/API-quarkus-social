e4095d68cdaa5eb3e3800a666d9ec22f5699112c
alanaafsc <alana.ingrid@ufpe.br>
2022-10-26 16:02:57 -0300

    Implementando o método de seguir um usuário

--- quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/FollowerResource.java
@@ -1,14 +1,15 @@
 package io.github.alanaafsc.quarkussocial.rest;
 
 import io.github.alanaafsc.quarkussocial.domain.model.Follower;
+import io.github.alanaafsc.quarkussocial.domain.model.User;
 import io.github.alanaafsc.quarkussocial.domain.repository.FollowerRepository;
 import io.github.alanaafsc.quarkussocial.domain.repository.UserRepository;
+import io.github.alanaafsc.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,5 +23,20 @@ public class FollowerResource {
     public FollowerResource(FollowerRepository repository, UserRepository userRepository) {
         this.repository = repository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    public Response followUser(@PathParam("userId") Long userId, FollowerRequest request){
+        User user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        var follower = userRepository.findById(request.getFollowerId());
+        var entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        repository.persist(entity);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
---
+++ quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/dto/FollowerRequest.java
@@ -0,0 +1,8 @@
+package io.github.alanaafsc.quarkussocial.rest.dto;
+
+import lombok.Data;
+
+@Data
+public class FollowerRequest {
+    private Long followerId;
+}
