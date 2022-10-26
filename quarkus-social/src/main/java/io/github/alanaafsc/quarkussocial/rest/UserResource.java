6bbf40e8fe4d62e0f8839bcfe336e25ae86b39e6
alanaafsc <alana.ingrid@ufpe.br>
2022-10-26 10:50:02 -0300

    Aplicando validações com Hibernate Validator

--- quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/UserResource.java
@@ -1,33 +1,58 @@
 package io.github.alanaafsc.quarkussocial.rest;
 
 import io.github.alanaafsc.quarkussocial.domain.model.User;
+import io.github.alanaafsc.quarkussocial.domain.repository.UserRepository;
 import io.github.alanaafsc.quarkussocial.rest.dto.CreateUserRequest;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    private UserRepository repository;
+    private Validator validator;
+
+    @Inject
+    public UserResource(UserRepository repository, Validator validator){
+        this.repository = repository;
+        this.validator = validator;
+    }
+
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest){
+
+        Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+        if(!violations.isEmpty()){
+            ConstraintViolation<CreateUserRequest> erro = violations.stream().findAny().get();
+            String errorMessage = erro.getMessage();
+            return Response.status(400).entity(errorMessage).build();
+        }
+
         User user = new User();
         user.setAge(userRequest.getAge());
         user.setName(userRequest.getName());
-        user.persist();
+
+        repository.persist(user);
+
         return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers(){
-        PanacheQuery<User> query = User.findAll();
+        PanacheQuery<User> query = repository.findAll();
         return Response.ok(query.list()).build();
     }
 
@@ -35,9 +60,10 @@ public class UserResource {
     @Path("{id}")
     @Transactional
     public Response deleteUser(@PathParam("id") Long id){
-        User user = User.findById(id);
+        User user = repository.findById(id);
+
         if(user != null){
-            user.delete();
+            repository.delete(user);
             return Response.ok().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
@@ -47,7 +73,7 @@ public class UserResource {
     @Path("{id}")
     @Transactional
     public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData){
-        User user = User.findById(id);
+        User user = repository.findById(id);
         if(user != null){
             user.setName(userData.getName());
             user.setAge(userData.getAge());
