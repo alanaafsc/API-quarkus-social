5504e5bf8310d0673c506cde72218209b4007042
alanaafsc <alana.ingrid@ufpe.br>
2022-10-27 11:19:02 -0300

    Finalizando testes do UserResource - testes ordenados

--- quarkus-social/src/test/java/io/github/alanaafsc/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/alanaafsc/quarkussocial/rest/UserResourceTest.java
@@ -2,11 +2,13 @@ package io.github.alanaafsc.quarkussocial.rest;
 
 import io.github.alanaafsc.quarkussocial.rest.dto.CreateUserRequest;
 import io.github.alanaafsc.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -14,9 +16,15 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
+
     @Test
     @DisplayName("Should create an user successfully")
+    @Order(1)
     public void createUserTest(){
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -26,7 +34,7 @@ class UserResourceTest {
                         contentType(ContentType.JSON).
                         body(user).
                 when()
-                        .post("/users")
+                        .post(apiURL)
                 .then()
                         .extract().response();
         assertEquals(201, response.statusCode());
@@ -34,6 +42,7 @@ class UserResourceTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Should return error when json is not valid")
     public void createUserValidationErrorTest(){
         var user = new CreateUserRequest();
@@ -44,7 +53,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(user)
                 .when()
-                        .post("/users")
+                        .post(apiURL)
                 .then()
                         .extract().response();
         assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
@@ -53,5 +62,18 @@ class UserResourceTest {
         List<Map<String, String>> errors = response.jsonPath().getList("errors");
         assertNotNull(errors.get(0).get("message"));
         assertNotNull(errors.get(1).get("message"));
+    }
+
+    @Test
+    @DisplayName("Should list all users")
+    @Order(3)
+    public void listAllUsersTest(){
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get(apiURL)
+        .then()
+            .statusCode(200)
+            .body("size()", Matchers.is(1));
     }
 }
