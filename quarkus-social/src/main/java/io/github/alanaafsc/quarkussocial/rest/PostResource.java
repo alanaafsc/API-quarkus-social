dfd657ba2ec832911cd1cffc8d90f81e4a952ed8
alanaafsc <alana.ingrid@ufpe.br>
2022-10-26 22:23:21 -0300

    Restringindo o acesso aos posts

--- quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/PostResource.java
+++ quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/rest/PostResource.java
@@ -2,6 +2,7 @@ package io.github.alanaafsc.quarkussocial.rest;
 
 import io.github.alanaafsc.quarkussocial.domain.model.Post;
 import io.github.alanaafsc.quarkussocial.domain.model.User;
+import io.github.alanaafsc.quarkussocial.domain.repository.FollowerRepository;
 import io.github.alanaafsc.quarkussocial.domain.repository.PostRepository;
 import io.github.alanaafsc.quarkussocial.domain.repository.UserRepository;
 import io.github.alanaafsc.quarkussocial.rest.dto.CreatePostRequest;
@@ -13,6 +14,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.lang.annotation.Repeatable;
 import java.util.stream.Collectors;
 
 @Path("/users/{userId}/posts")
@@ -22,11 +24,14 @@ public class PostResource {
 
     private UserRepository userRepository;
     private PostRepository repository;
+    private FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(UserRepository userRepository, PostRepository repository) {
+    public PostResource(UserRepository userRepository, PostRepository repository,
+                        FollowerRepository followerRepository) {
         this.userRepository = userRepository;
         this.repository = repository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -45,10 +50,27 @@ public class PostResource {
     }
 
     @GET
-    public Response listPosts(@PathParam("userId") Long userId){
+    public Response listPosts(@PathParam("userId") Long userId, @HeaderParam("followerId") Long followerId){
         User user = userRepository.findById(userId);
         if(user == null){
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        if(followerId == null){
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity("You forgot the header followerId").build();
+        }
+
+        User follower = userRepository.findById(followerId);
+        if(follower == null){
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity("inexistent followerId").build();
+        }
+
+        boolean follows = followerRepository.follows(follower, user);
+        if(!follows){
+            return Response.status(Response.Status.FORBIDDEN).
+                    entity("You can't see these posts").build();
         }
 
         var query = repository.
