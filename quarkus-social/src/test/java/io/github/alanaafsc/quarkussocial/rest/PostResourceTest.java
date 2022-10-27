0908a9f8b5049bd1faf2f7e4c74186f9d2e485ad
alanaafsc <alana.ingrid@ufpe.br>
2022-10-27 11:48:58 -0300

    Testes para PostResource - criando post

---
+++ quarkus-social/src/test/java/io/github/alanaafsc/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,53 @@
+package io.github.alanaafsc.quarkussocial.rest;
+
+import io.github.alanaafsc.quarkussocial.domain.model.User;
+import io.github.alanaafsc.quarkussocial.domain.repository.UserRepository;
+import io.github.alanaafsc.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+class PostResourceTest {
+
+    @Inject
+    UserRepository userRepository;
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    public void setUP(){
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("Should create a post for a user")
+    public void createPostTest(){
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+        
+        given()
+            .contentType(ContentType.JSON)
+            .body(postRequest)
+            .pathParam("userId", userId)
+        .when()
+            .post()
+        .then()
+            .statusCode(201);
+    }
+}
