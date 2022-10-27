c168adb0f217bed524eb7925a741afc561c70e4b
alanaafsc <alana.ingrid@ufpe.br>
2022-10-26 22:03:06 -0300

    Implementando o método para deixar de seguir um usuário

--- quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/domain/repository/FollowerRepository.java
+++ quarkus-social/src/main/java/io/github/alanaafsc/quarkussocial/domain/repository/FollowerRepository.java
@@ -18,7 +18,7 @@ public class FollowerRepository implements PanacheRepository<Follower> {
         var params = Parameters.with("follower", follower).
                 and("user", user).map();
 
-        PanacheQuery<Follower> query = find("follower =: follower and user =:user", params);
+        PanacheQuery<Follower> query = find("follower =:follower and user =:user", params);
         Optional<Follower> result = query.firstResultOptional();
 
         return result.isPresent();
@@ -27,5 +27,11 @@ public class FollowerRepository implements PanacheRepository<Follower> {
     public List<Follower> findByUser(Long userId){
         PanacheQuery<Follower> query = find("user.id", userId);
         return query.list();
+    }
+
+    public void deleteByFollowerAndUser(Long followerId, Long userId) {
+        var params = Parameters.with("userId", userId).
+                and("followerId", followerId).map();
+        delete("follower.id =:followerId and user.id =:userId", params);
     }
 }
